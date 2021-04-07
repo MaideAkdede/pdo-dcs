841daaa82c6ab030e5ad75c8552e8302ff8e3cec
MaideAkdede <maide.akdede@student.hepl.com>
2021-04-07 16:37:23 +0200

    :memo: Add Models

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
 require('./configs/config.php');
 require('./utils/dbaccess.php');
+require('./models/user.php');
+
 $connection = getConnection();
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    $userRequest = 'SELECT * FROM users WHERE id = :id';
-    $pdoSt = $connection->prepare($userRequest);
-    $pdoSt->execute([':id' => $id]);
-    $user = $pdoSt->fetch();
+    $user = find($connection, $id);
     $view = 'user.php';
 } else {
-    $usersRequest = 'SELECT * FROM users';
-    $pdoSt = $connection->query($usersRequest);
-    $users = $pdoSt->fetchAll();
+    $users = all($connection);
     $view = 'users.php';
 }
 require("./views/{$view}");
